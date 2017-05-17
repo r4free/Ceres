2449d432784dfa2e009ce8a7795a6e90f20dc00b
r4free@gmail.com <r4free@gmail.com>
2017-05-17 11:19:57 -0300

    estrutura

---
+++ src/Ceres/Controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Rafa
+ * Date: 17/05/2017
+ * Time: 11:17
+ */
+
+namespace Ceres\Controller;
+
+
+class Controller
+{
+public function index(){
+    echo "tudo ok";
+}
+}
